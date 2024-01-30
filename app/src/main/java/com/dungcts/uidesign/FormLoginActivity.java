9c5bc99ef2991a59f042da5452fe96f4b23b9cf8
Dung Paulus CTS <linhtam2001a@gmail.com>
2024-01-30 23:26:37 +0700

    update layout

---
+++ app/src/main/java/com/dungcts/uidesign/FormLoginActivity.java
@@ -0,0 +1,14 @@
+package com.dungcts.uidesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formlogin_linearlayout);
+    }
+}
---
+++ app/src/main/java/com/dungcts/uidesign/FormSignupActivity.java
@@ -0,0 +1,14 @@
+package com.dungcts.uidesign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formsignup_linearlayout);
+    }
+}
