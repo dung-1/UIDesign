d10974b7d75c54b41062e7e925d05b1f3d165efa
Dung Paulus CTS <linhtam2001a@gmail.com>
2024-02-23 21:24:34 +0700

    update layout signup and login

--- app/src/main/java/com/dungcts/uidesign/FormLoginActivity.java
+++ app/src/main/java/com/dungcts/uidesign/FormLoginActivity.java
@@ -9,6 +9,6 @@ public class FormLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formlogin_linearlayout);
+        setContentView(R.layout.activity_formsignup_linearlayout);
     }
 }
